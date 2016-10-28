78ca425e3850e4747cca74d31fdc521560fe0c94
Matt Traudt <sirmatt@ksu.edu>
2016-10-27 23:23:35 -0400

    Add internal permission check funcs to inventory

--- inventory.py
+++ inventory.py
@@ -14,6 +14,18 @@ class Inventory:
     def __init__(self):
         self.b58 = globals.base58_hashids
         self.encode_id = globals.encode_id
+    def __can_index(self, session):
+        return 'is_student' in session and session['is_student']
+
+    def __can_show(self, session):
+        return 'is_student' in session and session['is_student']
+
+    def __can_edit(self, session):
+        return 'is_officer' in session and session['is_officer']
+
+    def __can_update(self, session):
+        return 'is_officer' in session and session['is_officer']
+
     def index(self, request, session):
         if request.method == 'GET':
             if 'person_id' not in session:
