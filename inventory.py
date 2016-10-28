470973429fe389b00a3be92cfbd07bf43dd74393
Matt Traudt <sirmatt@ksu.edu>
2016-10-27 23:23:57 -0400

    Add internal db funcs to inventory

--- inventory.py
+++ inventory.py
@@ -14,6 +14,26 @@ class Inventory:
     def __init__(self):
         self.b58 = globals.base58_hashids
         self.encode_id = globals.encode_id
+
+    def __db_get_table(self, db):
+        i, _ = db.get_table("inventory")
+        return i
+
+    def __db_get_inventory(self):
+        with DatabaseConnection() as db:
+            inv = self.__db_get_table(db)
+            db.select([inv])
+            return [ self.encode_id(dict(row)) for row in db.fetchall() ]
+
+    def __db_get_item(self, id):
+        with DatabaseConnection() as db:
+            inv = self.__db_get_table(db)
+            db.select([inv], inv.c.id == id)
+            rows = [ self.encode_id(dict(row)) for row in db.fetchall() ]
+            if len(rows) != 1:
+                return None
+            return rows[0]
+
     def __can_index(self, session):
         return 'is_student' in session and session['is_student']
 
