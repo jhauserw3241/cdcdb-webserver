0e4a9d017a301bde9c6de4f04ece34506ae39530
Matt Traudt <sirmatt@ksu.edu>
2016-11-20 20:41:51 -0500

    Prevent this js from being run if on a different event page

--- static/events.js
+++ static/events.js
@@ -3,19 +3,21 @@ var r_is_competition = document.getElementById("is_competition");
 var r_is_general = document.getElementById("is_general");
 
 var form = document.getElementById("event_form");
-var meeting_items = form.getElementsByClassName("meeting");
-var competition_items = form.getElementsByClassName("competition");
+if (form) {
+    var meeting_items = form.getElementsByClassName("meeting");
+    var competition_items = form.getElementsByClassName("competition");
 
-r_is_meeting.onchange = enable_meeting;
-r_is_competition.onchange = enable_competition;
-r_is_general.onchange = enable_general;
+    r_is_meeting.onchange = enable_meeting;
+    r_is_competition.onchange = enable_competition;
+    r_is_general.onchange = enable_general;
 
-if (r_is_meeting.checked) {
-	enable_meeting();
-} else if (r_is_competition.checked) {
-	enable_competition();
-} else if (r_is_general.checked) {
-	enable_general();
+    if (r_is_meeting.checked) {
+        enable_meeting();
+    } else if (r_is_competition.checked) {
+        enable_competition();
+    } else if (r_is_general.checked) {
+        enable_general();
+    }
 }
 
 function enable_meeting() {
